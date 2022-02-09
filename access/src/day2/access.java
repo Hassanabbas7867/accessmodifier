b62a0e60662c3096a1ec45a225e4fafd40e3fce7
hassan abbas <hassan abbas@DESKTOP-RIL9KUS>
2022-02-09 18:26:04 +0530

    access

---
+++ access/src/day2/access.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class access{
+    int nos;
+    float hrs;
+    int noc;
+    void displayStudents() {//m1
+        System.out.println("Number of Students");
+    }
+    void displayHours() {//m2
+        System.out.println("Number of Hours");
+    }
+    void displayClass() {//m3
+        System.out.println("Number of Class");
+    }
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        access am = new access();
+        am.displayStudents();
+    }
+
+}
+
+
